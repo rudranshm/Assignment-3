6fbb64852726b61107ab7af73bc59216e12db95d
rudranshm <102405143+rudranshm@users.noreply.github.com>
2022-05-12 21:41:54 +0530

    Update Assignment3.py

--- Code and Documents/Python/Assignment3.py
+++ Code and Documents/Python/Assignment3.py
@@ -4,9 +4,9 @@ p3=1/6 #Probability of 5 appearing on the second toss
 
 Pe=p1 # E is the event that 4 appears on the third toss
 Pf=p2*p3 # F is the event that 6 and 5 appears respectively on first two tosses
-Peuf=Pe*Pf 
+PEnF=Pe*Pf 
 
 
-Pelf=Peuf/Pf # By formula
+Pelf=PEnF/Pf # By formula
 
 print(" The probabilty P(E/F) will be ",Pelf)
